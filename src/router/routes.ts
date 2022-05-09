aa2f7cc01431ed99313fcf3f7c49778315a85515
Dung Le <dzunglee@Dzungs-MBP-M1.local>
2022-05-09 09:33:33 +0700

    fix(sonar): resolved issues from sonar

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,15 @@
 const NotFound = () => import('modules/pages/views/404.vue')
 const Dashboard = () => import('modules/dashboard/views/index.vue')
+const Button = () => import('modules/dashboard/views/index.vue')
 const routes = [
   {
     path: '/',
     component: Dashboard,
+    name: 'Halana messenger',
+  },
+  {
+    path: '/buttons',
+    component: Button,
     name: 'Halana messenger',
   },
   {
