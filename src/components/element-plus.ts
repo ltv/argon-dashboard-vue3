7b65412c121deed0274680bc68183696920bdfb2
Dung Le <dzunglee@Dzungs-MBP-M1.local>
2022-05-09 09:57:44 +0700

    fix(sonar): resolved issues from sonar

--- src/components/element-plus.ts
+++ src/components/element-plus.ts
@@ -1,7 +1,5 @@
 // Custom css, do it later, now use the element plus css
 import 'assets/css/el-button.css'
-import 'assets/css/el-message.css'
-import 'assets/css/el-table.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/es/components/message/style/css'
 import 'element-plus/es/components/message-box/style/css'
