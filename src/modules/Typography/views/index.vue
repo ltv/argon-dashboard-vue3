4cc4ecd39211aa669de09737f6d4d239f6fd8628
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2022-05-12 10:49:22 +0700

    conflict

---
+++ src/modules/Typography/views/index.vue
@@ -0,0 +1,62 @@
+<template> 
+  <!-- <p style="font-size: var(--el-font-size-extra-large)">This is another demo</p> -->
+  <div class="bg-white w-3/4 mx-auto mt-5 shadow-lg rounded-lg">
+      <div class="p-5 border-b-2">
+          <h1 style="font-size: var(--el-font-size-extra-large)">Headings</h1>
+      </div>
+      <div class="p-5">
+          <div class="flex w-full mb-5 leading-6">
+              <h2 class="w-1/4 opacity-50 uppercase text-sm"
+                style="font-size: var(--el-font-size-base)"
+              >Heading 1</h2>
+              <p class="w-3/4"
+                style="font-size: var(--el-font-size-extra-large)"
+              >Argon Dashboard PRO React</p>
+          </div>
+          <div class="flex w-full mb-5 leading-6">
+              <h2 class="w-1/4 opacity-50 uppercase"
+                style="font-size: var(--el-font-size-base)"
+              >Heading 2</h2>
+              <p class="w-3/4"
+                style="font-size: var(--el-font-size-large)"
+              >Argon Dashboard PRO React</p>
+          </div>
+          <div class="flex w-full mb-5 leading-6">
+              <h2 class="w-1/4 opacity-50 uppercase"
+                style="font-size: var(--el-font-size-base)"
+              >Heading 3</h2>
+              <p class="w-3/4"
+                style="font-size: var(--el-font-size-medium)"
+              >Argon Dashboard PRO React</p>
+          </div>
+          <div class="flex w-full mb-5 leading-6">
+              <h2 class="w-1/4 opacity-50 uppercase"
+                style="font-size: var(--el-font-size-base)"
+              >Heading 4</h2>
+              <p class="w-3/4"
+                style="font-size: var(--el-font-size-base)"
+              >Argon Dashboard PRO React</p>
+          </div>
+          <div class="flex w-full mb-5 leading-6">
+              <h2 class="w-1/4 opacity-50 uppercase"
+                style="font-size: var(--el-font-size-base)"
+              >Heading 5</h2>
+              <p class="w-3/4"
+                style="font-size: var(--el-font-size-small)"
+              >Argon Dashboard PRO React</p>
+          </div>
+          <div class="flex w-full mb-5 leading-6">
+              <h2 class="w-1/4 opacity-50 uppercase"
+                style="font-size: var(--el-font-size-base)"
+              >Heading 6</h2>
+              <p class="w-3/4"
+                style="font-size: var(--el-font-size-extra-small)"
+              >Argon Dashboard PRO React</p>
+          </div>             
+      </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,15 @@
 const NotFound = () => import('modules/pages/views/404.vue')
 const Dashboard = () => import('modules/dashboard/views/index.vue')
 const Button = () => import('modules/dashboard/views/index.vue')
+<<<<<<< HEAD
 const Table = () => import('modules/table/views/index.vue')
 const Grid = () => import('modules/grid/views/index.vue')
 const Notification = () => import('modules/notification/views/index.vue')
+=======
+const Table = () => import('modules/Table/views/index.vue')
+const Grid = () => import('modules/Grid/views/index.vue')
+const Typography = () => import('modules/Typography/views/index.vue')
+>>>>>>> edaf510 (typography)
 const routes = [
   {
     path: '/',
@@ -29,6 +35,11 @@ const routes = [
     path: '/grid',
     component: Grid,
     name: 'Grid',
+  },
+  {
+    path: '/typography',
+    component: Typography,
+    name: 'Typography',
   },
   {
     path: '/:pathMatch(.*)*',
