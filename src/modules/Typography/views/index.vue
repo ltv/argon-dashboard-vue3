b7a9d78f4501156ad4b20399ae8eb094253d793e
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2022-05-12 16:35:32 +0700

    finished

--- src/modules/Typography/views/index.vue
+++ src/modules/Typography/views/index.vue
@@ -1,23 +1,342 @@
-<template> 
-  <div class="bg-white w-3/4 mx-auto mt-5 shadow-lg rounded-lg">
-      <div class="p-5 border-b-2">
-          <h1>Headings</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          <h4>Heading 4</h4>
-          <h5>Heading 5</h5>
-          <h6>Heading 6</h6>
+<template>
+  <!-- Headings -->
+  <div class="bg-white w-1/2 mx-auto mt-5 mb-2 shadow-lg rounded-lg flex flex-col">
+    <!-- Heading header -->
+    <div class="p-5 border-b-2">
+      <p class="text-lg font-sans font-bold">Headings</p>
+    </div>
+    <div class="p-5">
+      <!-- H1 -->
+      <div class="flex justify-center items-center h-10 w-full mt-5 mb-5">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading 1
+          </p>
+        </div>
+        <div class="w-3/4">
+          <h1>Argon Dashboard PRO React</h1>
+        </div>
       </div>
-      <div class="p-5">
-          <div class="flex w-full mb-5 leading-6">
-              <h2 class="w-1/4 opacity-50 uppercase text-sm">Heading 1</h2>
-              <p class="w-3/4">Argon Dashboard PRO React</p>
-          </div>
-                     
+      <!-- H2 -->
+      <div class="flex justify-center items-center h-10 w-full mt-5 mb-5">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading 2
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h2>Argon Dashboard PRO React</h2>
+        </div>
       </div>
+      <!-- H3 -->
+      <div class="flex justify-center items-center h-10 w-full mt-5 mb-5">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading 3
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h3>Argon Dashboard PRO React</h3>
+        </div>
+      </div>
+      <!-- H4 -->
+      <div class="flex justify-center items-center h-10 w-full mt-5 mb-5">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading 4
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h4>Argon Dashboard PRO React</h4>
+        </div>
+      </div>
+      <!-- H5 -->
+      <div class="flex justify-center items-center h-10 w-full mt-5 mb-5">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading 5
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h5>Argon Dashboard PRO React</h5>
+        </div>
+      </div>
+      <!-- H6 -->
+      <div class="flex justify-center items-center h-10 w-full mt-5 mb-5">
+        <div class="w-1/4 py-1 flex">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading 6
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h6>Argon Dashboard PRO React</h6>
+        </div>
+      </div>
+    </div>
   </div>
+  <!-- End Headings -->
+
+  <!-- Display titles -->
+  <div class="bg-white w-1/2 mx-auto mt-5 shadow-lg rounded-lg flex flex-col">
+    <!-- Display header -->
+    <div class="p-5 border-b-2">
+      <p class="text-lg font-sans font-bold">Display titles</p>
+    </div>
+    <div class="p-5">
+      <!-- Display1 -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Display 1
+          </p>
+        </div>
+        <div class="w-3/4">
+          <h1 class="display1">Argon Dashboard PRO React</h1>
+        </div>
+      </div>
+      <!-- Display2 -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Display 2
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h2 class="display2">Argon Dashboard PRO React</h2>
+        </div>
+      </div>
+      <!-- Display3 -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Display 3
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h3 class="display3">Argon Dashboard PRO React</h3>
+        </div>
+      </div>
+      <!-- Display4 -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Display 4
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h4 class="display4">Argon Dashboard PRO React</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End display titles -->
+
+  <!-- Specialized titles -->
+  <div class="bg-white w-1/2 mx-auto mt-5 shadow-lg rounded-lg flex flex-col">
+    <!-- Specialized header -->
+    <div class="p-5 border-b-2">
+      <p class="text-lg font-sans font-bold">Specialized titles</p>
+    </div>
+    <div class="p-5">
+      <!-- Heading -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading
+          </p>
+        </div>
+        <div class="w-3/4">
+          <h1 class="heading">Argon Dashboard PRO React</h1>
+        </div>
+      </div>
+      <!-- Heading title -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading title
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h2 class="heading_title">Argon Dashboard PRO React</h2>
+        </div>
+      </div>
+      <!-- Heading with small subtitle -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold"></p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h3 class="heading_sub_title">Header with small subtitle</h3>
+        </div>
+      </div>
+      <!-- Heading section -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4 py-1">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Heading section
+          </p>
+        </div>
+        <div class="w-3/4 py-1">
+          <h4 class="heading_section">
+            According to the National Oceanic and Atmospheric Administration,
+            Ted, Scambos, NSIDClead scentist, puts the potentially record
+            maximum.
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End display titles -->
+
+  <!-- Specialized -->
+  <div class="bg-white w-1/2 mx-auto mt-5 shadow-lg rounded-lg flex flex-col">
+    <!-- Paragraphs Header -->
+    <div class="p-5 border-b-2">
+      <p class="text-lg font-sans font-bold">Paragraphs</p>
+    </div>
+    <div class="p-5">
+      <!-- Paragraphs -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Paragraphs
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers. I understand culture. I am
+            the nucleus. I think that’s a responsibility that I have, to push
+            possibilities, to show people, this is the level that things could
+            be at.
+          </p>
+        </div>
+      </div>
+      <!-- Lead text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Lead text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="lead">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers. I understand culture. I am
+            the nucleus. I think that’s a responsibility that I have, to push
+            possibilities, to show people, this is the level that things could
+            be at.
+          </p>
+        </div>
+      </div>
+      <!-- Quote -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Quote
+          </p>
+        </div>
+        <div class="w-3/4">
+          <blockquote class="blockquote">
+            <p class="mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.
+            </p>
+            <footer class="blockquote-footer">
+              --- Someone famous in
+              <cite title="Source Title">Source Title</cite>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+      <!-- Muted text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Muted text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="text-muted mb-0">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </p>
+        </div>
+      </div>
+      <!-- Primary text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Primary text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="text-primary mb-0">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </p>
+        </div>
+      </div>
+      <!-- Info text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Info text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="text-info mb-0">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </p>
+        </div>
+      </div>
+      <!-- Success text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Success text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="text-success mb-0">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </p>
+        </div>
+      </div>
+      <!-- Warning text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Warning text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="text-warning mb-0">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </p>
+        </div>
+      </div>
+      <!-- Danger text -->
+      <div class="flex justify-center items-center h-30 w-full mt-10 mb-10">
+        <div class="w-1/4">
+          <p class="opacity-30 uppercase text-sm font-sans font-semibold">
+            Danger text
+          </p>
+        </div>
+        <div class="w-3/4">
+          <p class="text-danger mb-0">
+            I will be the leader of a company that ends up being worth billions
+            of dollars, because I got the answers...
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End paragraphs -->
 </template>
 
 <script lang="ts" setup>
-
 </script>
