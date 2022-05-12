7f6cd99188e2218eeb865b4353105d4e461d5120
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2022-05-12 20:07:51 +0700

    fix bug

--- src/components/element-plus.ts
+++ src/components/element-plus.ts
@@ -1,5 +1,4 @@
 // Custom css, do it later, now use the element plus css
-import 'assets/css/el-button.css'
 import 'assets/css/el-table.css'
 import 'assets/css/el-table-column.css'
 import 'assets/css/el-dropdown.css'
