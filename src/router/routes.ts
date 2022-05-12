7eb059984046ef9be0ff78ba854965b33d97fc83
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2022-05-12 11:34:18 +0700

    resole

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,15 +1,8 @@
 const NotFound = () => import('modules/pages/views/404.vue')
 const Dashboard = () => import('modules/dashboard/views/index.vue')
-<<<<<<< HEAD
-const Button = () => import('modules/dashboard/views/index.vue')
-const Table = () => import('modules/table/views/index.vue')
-const Grid = () => import('modules/grid/views/index.vue')
-const Notification = () => import('modules/notification/views/index.vue')
-=======
 const Button = () => import('modules/Button/views/index.vue')
 const Table = () => import('modules/Table/views/index.vue')
 const Grid = () => import('modules/Grid/views/index.vue')
->>>>>>> 96ee44a (styleing)
 const routes = [
   {
     path: '/',
