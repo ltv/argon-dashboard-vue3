6f80a852f4226cd53b9aebff6085a665b967cae5
LeThanhPhongLTV <phong.lt@ltv.vn>
2022-05-12 13:40:03 +0700

    resolve conflict

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,9 @@
 const NotFound = () => import('modules/pages/views/404.vue')
 const Dashboard = () => import('modules/dashboard/views/index.vue')
-const Button = () => import('modules/Button/views/index.vue')
-const Table = () => import('modules/Table/views/index.vue')
-const Grid = () => import('modules/Grid/views/index.vue')
+const Button = () => import('modules/dashboard/views/index.vue')
+const Table = () => import('modules/table/views/index.vue')
+const Grid = () => import('modules/grid/views/index.vue')
+const Notification = () => import('modules/notification/views/index.vue')
 const routes = [
   {
     path: '/',
