a16973181bbc2afdfbe4d041ec2d6e4a31f46596
LeThanhPhongLTV <phong.lt@ltv.vn>
2022-05-16 22:14:09 +0700

    fix grid

--- src/modules/grid/views/index.vue
+++ src/modules/grid/views/index.vue
@@ -2,12 +2,12 @@
   <div class="p-6 bg-zinc-50 w-full">
     <div class="block w-4/5 h-auto mx-auto">
       <div class="flex">
-        <div class="container mx-auto p-4 border shadow-md drop-shadow-sm rounded">
-          <div class="font-base text-lg text-indigo-900">
+        <div class="container mx-auto border shadow-md drop-shadow-sm rounded">
+          <div class="font-base text-lg text-indigo-900 p-4">
             Grid system
           </div>
           <el-divider />
-          <div class="grid grid-cols-5 gap-4 text-center">
+          <div class="grid grid-cols-5 gap-6 mb-4 p-4 mb-4 text-center">
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">One of five columns</div>
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">One of five columns</div>
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">One of five columns</div>
@@ -23,7 +23,7 @@
             Equal-width
           </div>
           <el-divider />
-          <div class="grid grid-cols-6 gap-4 text-center">
+          <div class="grid grid-cols-6 gap-6 mb-4 p-4 text-center">
             <div class="py-2 border border-slate-300 rounded col-span-3 drop-shadow-sm shadow">1 of 2</div>
             <div class="py-2 border border-slate-300 rounded col-span-3 drop-shadow-sm shadow">1 of 2</div>
 
@@ -41,7 +41,7 @@
             Setting one column width
           </div>
           <el-divider />
-          <div class="grid grid-cols-5 gap-4 text-center">
+          <div class="grid grid-cols-5 gap-6 mb-4 p-4  text-center">
             <div class="py-2 border border-slate-300 rounded col-span-2 drop-shadow-sm shadow">1 of 3</div>
             <div class="py-2 border border-slate-300 rounded w-32 drop-shadow-sm shadow">2 of 3 (smaller)</div>
             <div class="py-2 border border-slate-300 rounded col-span-2 drop-shadow-sm shadow">3 of 3</div>
@@ -63,7 +63,7 @@
             Variable width content
           </div>
           <el-divider />
-          <div class="grid grid-cols-6 gap-4 text-center">
+          <div class="grid grid-cols-6 gap-6 mb-4 p-4  text-center">
             <div class="py-5 border border-slate-300 rounded col-end-3 drop-shadow-sm shadow">01</div>
             <div class="py-5 border border-slate-300 rounded col-start-3 col-span-2 drop-shadow-sm shadow">02</div>
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">Variable width content</div>
@@ -86,7 +86,7 @@
             Equal-width multi-row
           </div>
           <el-divider />
-          <div class="grid grid-cols-3 gap-4 text-center">
+          <div class="grid grid-cols-3 gap-6 mb-4 p-4  text-center">
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">01</div>
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">02</div>
             <div class="py-2 border border-slate-300 rounded drop-shadow-sm shadow">03</div>
@@ -109,7 +109,7 @@
             Mix and match
           </div>
           <el-divider />
-          <div class="grid grid-cols-6 gap-4 text-center">
+          <div class="grid grid-cols-6 gap-6 mb-4 p-4  text-center">
             <div class="py-5 border border-slate-300 rounded md:col-span-1 sm:col-span-3 drop-shadow-sm shadow">md:col-span-1 sm:col-span-3</div>
             <div class="py-5 border border-slate-300 rounded md:col-span-1 sm:col-span-2 drop-shadow-sm shadow">md:col-span-1 sm:col-span-2</div>
             <div class="py-7 border border-slate-300 rounded md:col-span-4 sm:col-span-1 drop-shadow-sm shadow">md:col-span-4 sm:col-span-1</div>
