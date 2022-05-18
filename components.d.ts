5de06c98b26484039f330b9674b09d952cc73e2a
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2022-05-18 16:03:19 +0700

    pull main

--- components.d.ts
+++ components.d.ts
@@ -17,6 +17,7 @@ declare module 'vue' {
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
     ElLink: typeof import('element-plus/es')['ElLink']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSkeleton: typeof import('element-plus/es')['ElSkeleton']
@@ -26,6 +27,8 @@ declare module 'vue' {
     ElTag: typeof import('element-plus/es')['ElTag']
     Grid: typeof import('./src/components/Grid/index.vue')['default']
     Main: typeof import('./src/components/Main/index.vue')['default']
+    Navigation: typeof import('./src/components/Navigation/index.vue')['default']
+    Sidebar: typeof import('./src/components/Sidebar/index.vue')['default']
     Table: typeof import('./src/components/Table/index.vue')['default']
   }
 }
