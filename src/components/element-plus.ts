9cc35c95cff331ab673e95579311fda9e86bd8f8
Trung Hieu <dangtrunghieusg@gmail.com>
2022-05-19 22:05:30 +0700

    feat: [Component] - Typography | Design layout & Reponsive

--- src/components/element-plus.ts
+++ src/components/element-plus.ts
@@ -1,5 +1,6 @@
 // Custom css, do it later, now use the element plus css
 import 'assets/css/el-button.scss'
+import 'assets/css/typography.scss'
 
 import 'element-plus/dist/index.css'
 import 'element-plus/es/components/message/style/css'
