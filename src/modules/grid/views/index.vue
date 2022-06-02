71a6aabda9ba21082296d83281c28cb6c6326a8b
Dang Trung Hieu <82331523+Trung-Hieu-Dev@users.noreply.github.com>
2022-06-02 18:38:17 +0700

    feat(Grid): CSS & Responsive | JIRA: PT-100 (#55)
    Co-authored-by: Trung-Hieu-Dev <dangtrunghieusg.com>

--- src/modules/grid/views/index.vue
+++ src/modules/grid/views/index.vue
@@ -1,206 +1,89 @@
 <template>
-  <div class="w-full block w-full mx-auto h-auto w-full lg:max-w-2/3 py-2.5">
-    <div class="flex flex-wrap flex-col bg-white shadow mb-7 mx-auto rounded-lg">
-      <div class="p-5 border-b border-primary-white">
-        <h3 class="cursor-auto">Grid system</h3>
-      </div>
-      <div class="px-5 py-6">
-        <div class="md:grid grid-cols-3 gap-6 text-center font-thin">
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-md shadow">
-            One of three columns
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-md shadow">
-            One of three columns
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-md shadow">
-            One of three columns
-          </div>
-        </div>
+  <div class="flex flex-col gap-2 mx-auto h-auto w-full lg:w-2/3">
+    <!-- Grid system -->
+    <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
+      <div class="font-semibold text-lg text-indigo-900 px-6 pt-4">Grid system</div>
+      <el-divider />
+      <div class="grid grid-cols-1 sm:grid-cols-3 gap-7 mb-6 text-center items-center px-6 py-2">
+        <div class="grid-items">One of three columns</div>
+        <div class="grid-items">One of three columns</div>
+        <div class="grid-items">One of three columns</div>
       </div>
     </div>
 
-    <div class="flex pt-8">
-      <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
-        <div class="font-semibold text-lg text-indigo-900 px-4 pt-4">Equal-width</div>
-        <el-divider />
-        <div class="grid grid-cols-6 gap-6 px-8 mb-6 text-center items-center">
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-3 drop-shadow-md shadow">
-            1 of 2
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-3 drop-shadow-md shadow">
-            1 of 2
-          </div>
+    <!-- Equal-width -->
+    <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
+      <div class="font-semibold text-lg text-indigo-900 px-6 pt-4">Equal-width</div>
+      <el-divider />
+      <div class="grid grid-cols-6 gap-7 mb-6 text-center items-center px-6 py-2">
+        <div class="grid-items col-span-3">1 of 2</div>
+        <div class="grid-items col-span-3">2 of 2</div>
 
-          <div class="py-2 border border-slate-300 rounded col-span-2 drop-shadow-md shadow">
-            1 of 3
-          </div>
-          <div class="py-2 border border-slate-300 rounded col-span-2 drop-shadow-md shadow">
-            1 of 3
-          </div>
-          <div class="py-2 border border-slate-300 rounded col-span-2 drop-shadow-md shadow">
-            1 of 3
-          </div>
-        </div>
+        <div class="grid-items col-span-2">1 of 3</div>
+        <div class="grid-items col-span-2">2 of 3</div>
+        <div class="grid-items col-span-2">3 of 3</div>
       </div>
     </div>
 
-    <div class="flex pt-8">
-      <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
-        <div class="font-semibold text-lg px-4 pt-4 text-indigo-900">Setting one column width</div>
-        <el-divider />
-        <div class="grid grid-cols-6 gap-6 px-8 mb-6 text-center">
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-1 drop-shadow-md shadow">
-            1 of 3
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-3 drop-shadow-md shadow">
-            2 of 3 (wider)
-          </div>
-          <div
-            class="py-2 my-2 border border-slate-300 rounded col-start-6 col-span-1 drop-shadow-md shadow"
-          >
-            3 of 3
-          </div>
-
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-4 drop-shadow-md shadow">
-            1 of 3 (wider)
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-1 drop-shadow-md shadow">
-            2 of 3
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-1 drop-shadow-md shadow">
-            3 of 3
-          </div>
-
-          <div class="py-2 my-2 border border-slate-300 rounded col-span-3 drop-shadow-md shadow">
-            1 of 3 (wider)
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-md shadow">2 of 3</div>
-          <div
-            class="py-2 my-2 border border-slate-300 rounded col-start-5 col-span-2 drop-shadow-md shadow"
-          >
-            3 of 3 (wider)
-          </div>
-        </div>
+    <!-- Setting one column width -->
+    <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
+      <div class="font-semibold text-lg text-indigo-900 px-6 pt-4">Setting one column width</div>
+      <el-divider />
+      <div class="flex gap-4 text-center px-6 py-2">
+        <div class="grid-items flex-auto">1 of 3</div>
+        <div class="grid-items flex-auto w-2/4">2 of 3 (wider)</div>
+        <div class="grid-items flex-auto">3 of 3</div>
+      </div>
+      <div class="flex gap-4 mb-6 text-center px-6 py-2">
+        <div class="grid-items flex-auto">1 of 3</div>
+        <div class="grid-items flex-auto w-2/5">2 of 3 (wider)</div>
+        <div class="grid-items flex-auto">3 of 3</div>
       </div>
     </div>
 
-    <div class="flex pt-8">
-      <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
-        <div class="font-semibold text-lg px-4 pt-4 text-indigo-900">Variable width content</div>
-        <el-divider />
-        <div class="sm:grid grid-cols-6 gap-6 px-8 mb-6 text-center">
-          <div class="py-5 my-2 border border-slate-300 rounded col-end-3 drop-shadow-sm shadow">
-            01
-          </div>
-          <div
-            class="py-5 my-2 border border-slate-300 rounded col-start-3 col-span-2 drop-shadow-sm shadow"
-          >
-            02
-          </div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow w-fit">
-            Variable width content
-          </div>
+    <!-- Variable width content -->
+    <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
+      <div class="font-semibold text-lg text-indigo-900 px-6 pt-4">Variable width content</div>
+      <el-divider />
+      <div class="grid grid-cols-1 md:grid-cols-12 gap-7 mb-6 text-center px-6 py-2">
+        <div class="grid-items md:col-start-3 md:col-span-2">1 of 3</div>
+        <div class="grid-items md:col-span-3">Variable width content</div>
+        <div class="grid-items md:col-span-2">3 of 3</div>
 
-          <div class="py-5 my-2 border border-slate-300 rounded col-start-2 drop-shadow-sm shadow">
-            01
-          </div>
-          <div
-            class="py-2 my-2 border border-slate-300 rounded col-start-3 col-auto drop-shadow-sm shadow w-fit"
-          >
-            Variable width content
-          </div>
-          <div class="py-5 my-2 border border-slate-300 rounded col-start-4 drop-shadow-sm shadow">
-            03
-          </div>
-
-          <div
-            class="py-2 my-2 border border-slate-300 rounded col-start-1 drop-shadow-sm shadow w-fit"
-          >
-            Variable width content
-          </div>
-          <div class="py-5 my-2 border border-slate-300 rounded col-span-3 drop-shadow-sm shadow">
-            02
-          </div>
-          <div class="py-5 my-2 border border-slate-300 rounded col-span-2 drop-shadow-sm shadow">
-            03
-          </div>
-        </div>
+        <div class="grid-items md:col-span-7">1 of 3</div>
+        <div class="grid-items md:col-span-3">Variable width content</div>
+        <div class="grid-items md:col-span-2">3 of 3</div>
       </div>
     </div>
 
-    <div class="flex pt-8">
-      <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
-        <div class="font-semibold text-lg px-4 pt-4 text-indigo-900">Equal-width multi-row</div>
-        <el-divider />
-        <div class="grid grid-cols-3 gap-6 px-8 mb-6 text-center">
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">01</div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">02</div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">03</div>
-
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">01</div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">02</div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">03</div>
-
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">01</div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">02</div>
-          <div class="py-2 my-2 border border-slate-300 rounded drop-shadow-sm shadow">03</div>
-        </div>
+    <!-- Equal-width multi-row -->
+    <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
+      <div class="font-semibold text-lg text-indigo-900 px-6 pt-4">Equal-width multi-row</div>
+      <el-divider />
+      <div class="flex gap-4 text-center px-6 py-2">
+        <div class="grid-items flex-auto">col</div>
+        <div class="grid-items flex-auto">col</div>
+      </div>
+      <div class="flex gap-4 mb-6 text-center px-6 py-2">
+        <div class="grid-items flex-auto">col</div>
+        <div class="grid-items flex-auto">col</div>
       </div>
     </div>
 
-    <div class="flex pt-8">
-      <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
-        <div class="font-semibold text-lg px-4 pt-4 text-indigo-900">Mix and match</div>
-        <el-divider />
-        <div class="flex flex-wrap md:grid grid-cols-6 gap-6 px-8 mb-6 text-center">
-          <div
-            class="py-5 my-2 border border-slate-300 rounded md:col-span-1 sm:col-span-3 drop-shadow-sm shadow"
-          >
-            md:col-span-1 sm:col-span-3
-          </div>
-          <div
-            class="py-5 my-2 border border-slate-300 rounded md:col-span-1 sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-1 sm:col-span-2
-          </div>
-          <div
-            class="py-7 my-2 border border-slate-300 rounded md:col-span-4 sm:col-span-1 drop-shadow-sm shadow"
-          >
-            md:col-span-4 sm:col-span-1
-          </div>
+    <!-- Mix and match -->
+    <div class="container h-auto mx-auto border border-slate-50 bg-white shadow-lg mt-5 rounded">
+      <div class="font-semibold text-lg text-indigo-900 px-6 pt-4">Mix and match</div>
+      <el-divider />
+      <div class="grid grid-cols-1 sm:grid-cols-12 gap-6 mb-6 text-center px-6 py-2">
+        <div class="grid-items col-span-8">.col-6 .col-md-8</div>
+        <div class="grid-items col-span-4">.col-6 .col-md-4</div>
 
-          <div
-            class="py-5 my-2 border border-slate-300 rounded md:col-span-3 sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-3 sm:col-span-2
-          </div>
-          <div
-            class="py-5 my-2 border border-slate-300 rounded md:col-span-2 sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-2 sm:col-span-2
-          </div>
-          <div
-            class="py-2 my-2 border border-slate-300 rounded md:col-span-1 sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-1 sm:col-span-2
-          </div>
+        <div class="grid-items col-span-4">.col-6 .col-md-4</div>
+        <div class="grid-items col-span-4">.col-6 .col-md-4</div>
+        <div class="grid-items col-span-4">.col-6 .col-md-4</div>
 
-          <div
-            class="py-5 my-2 border border-slate-300 rounded sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-2
-          </div>
-          <div
-            class="py-5 my-2 border border-slate-300 rounded sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-2
-          </div>
-          <div
-            class="py-5 my-2 border border-slate-300 rounded sm:col-span-2 drop-shadow-sm shadow"
-          >
-            md:col-span-2
-          </div>
-        </div>
+        <div class="grid-items col-span-6">.col-6 .col-md-6</div>
+        <div class="grid-items col-span-6">.col-6 .col-md-6</div>
       </div>
     </div>
   </div>
@@ -216,3 +99,9 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+.grid-items {
+  @apply p-3 my-1 bg-white border border-slate-200 rounded drop-shadow-[0_4px_16px_rgba(0,0,0,0.10)] shadow-sm text-left text-sm font-normal;
+}
+</style>
