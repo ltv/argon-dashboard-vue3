d31f0e5e4c99f44adcf2caf3635bc821796da1d2
Trung-Hieu-Dev <dangtrunghieusg.com>
2022-06-04 11:30:02 +0700

    feat(typography): Fix UI | JIRA PT-131

--- src/modules/typography/views/index.vue
+++ src/modules/typography/views/index.vue
@@ -1,282 +1,255 @@
 <template>
-  <div class="w-full sm:w-2/3 md:w-2/3 mx-auto py-2">
+  <div class="w-full mx-auto lg:w-[65.95%]">
     <!-- Heading section -->
-    <div class="card-typography">
-      <div class="card-header">
-        <h3 class="text-lg">Headings</h3>
-      </div>
-      <div class="p-5">
-        <!-- Heading 1 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading 1</span>
+    <div class="container py-4 mx-auto">
+      <el-card class="border-slate-100" :body-style="{ padding: '0px' }">
+        <div>
+          <div class="card-header px-6 py-5">
+            <h3>Headings</h3>
           </div>
-          <div class="md:w-3/4">
-            <h1>Fancy Dashboard</h1>
+          <p class="break-normal text-slate-500 px-4"></p>
+          <div class="p-6">
+            <!-- Heading 1 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Heading 1</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h1>Fancy Dashboard</h1>
+              </div>
+            </div>
+            <!-- Heading 2 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Heading 2</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h2>Fancy Dashboard</h2>
+              </div>
+            </div>
+            <!-- Heading 3 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Heading 3</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h3>Fancy Dashboard</h3>
+              </div>
+            </div>
+            <!-- Heading 4 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Heading 4</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h4>Fancy Dashboard</h4>
+              </div>
+            </div>
+            <!-- Heading 5 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Heading 5</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h5>Fancy Dashboard</h5>
+              </div>
+            </div>
+            <!-- Heading 6 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Heading 6</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h6>Fancy Dashboard</h6>
+              </div>
+            </div>
           </div>
         </div>
-        <!-- Heading 2 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading 2</span>
-          </div>
-          <div class="md:w-3/4">
-            <h2>Fancy Dashboard</h2>
-          </div>
-        </div>
-        <!-- Heading 3 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading 3</span>
-          </div>
-          <div class="md:w-3/4">
-            <h3>Fancy Dashboard</h3>
-          </div>
-        </div>
-        <!-- Heading 4 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading 4</span>
-          </div>
-          <div class="md:w-3/4">
-            <h4>Fancy Dashboard</h4>
-          </div>
-        </div>
-        <!-- Heading 5 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading 5</span>
-          </div>
-          <div class="md:w-3/4">
-            <h5>Fancy Dashboard</h5>
-          </div>
-        </div>
-        <!-- Heading 6 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading 6</span>
-          </div>
-          <div class="md:w-3/4">
-            <h6>Fancy Dashboard</h6>
-          </div>
-        </div>
-      </div>
+      </el-card>
     </div>
 
-    <!-- Display section -->
-    <div class="card-typography">
-      <div class="card-header">
-        <h3 class="text-lg">Display titles</h3>
-      </div>
-      <div class="p-5">
-        <!-- Display 1 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Display 1</span>
+    <!-- Display titles -->
+    <div class="container py-4 mx-auto">
+      <el-card class="border-slate-100" :body-style="{ padding: '0px' }">
+        <div>
+          <div class="card-header px-6 py-5">
+            <h3>Display titles</h3>
           </div>
-          <div class="md:w-3/4">
-            <h1 class="display-1">Fancy Dashboard</h1>
+          <p class="break-normal text-slate-500 px-4"></p>
+          <div class="p-6">
+            <!-- Display 1 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Display 1</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h1 class="display-1">Fancy Dashboard</h1>
+              </div>
+            </div>
+            <!-- Display 2 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Display 2</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h2 class="display-2">Fancy Dashboard</h2>
+              </div>
+            </div>
+            <!-- Heading 3 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Display 3</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h3 class="display-3">Fancy Dashboard</h3>
+              </div>
+            </div>
+            <!-- Heading 4 -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Display 4</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <h4 class="display-4">Fancy Dashboard</h4>
+              </div>
+            </div>
           </div>
         </div>
-        <!-- Display 2 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Display 2</span>
-          </div>
-          <div class="md:w-3/4">
-            <h2 class="display-2">Fancy Dashboard</h2>
-          </div>
-        </div>
-        <!-- Heading 3 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Display 3</span>
-          </div>
-          <div class="md:w-3/4">
-            <h3 class="display-3">Fancy Dashboard</h3>
-          </div>
-        </div>
-        <!-- Heading 4 -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Display 4</span>
-          </div>
-          <div class="md:w-3/4">
-            <h4 class="display-4">Fancy Dashboard</h4>
-          </div>
-        </div>
-      </div>
+      </el-card>
     </div>
 
     <!-- Specialized titles -->
-    <div class="card-typography">
-      <div class="card-header">
-        <h3 class="text-lg">Specialized titles</h3>
-      </div>
-      <div class="p-5">
-        <!-- Heading -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading</span>
+    <div class="container py-4 mx-auto">
+      <el-card class="border-slate-100" :body-style="{ padding: '0px' }">
+        <div>
+          <div class="card-header px-6 py-5">
+            <h3>Paragraphs</h3>
           </div>
-          <div class="md:w-3/4">
-            <h3 class="heading">Fancy Dashboard</h3>
-          </div>
-        </div>
-        <!-- Heading title -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading Title</span>
-          </div>
-          <div class="md:w-3/4">
-            <h2 class="heading-title text-warning">Fancy Dashboard</h2>
-          </div>
-        </div>
-
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Heading Seaction</span>
-          </div>
-          <div class="md:w-3/4">
-            <div class="mb-2">
-              <h3 class="display-3">Header with small subtitle</h3>
+          <p class="break-normal text-slate-500 px-4"></p>
+          <div class="p-6">
+            <!-- Paragraphs -->
+            <div class="row medium mb-5">
+              <div class="md:w-1/4">
+                <span class="title block">Paragraph</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p>
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers. I understand culture. I am the nucleus. I think that’s
+                  a responsibility that I have, to push possibilities, to show people, this is the
+                  level that things could be at.
+                </p>
+              </div>
             </div>
-            <div class="mt-6 mb-4">
-              <h4 class="lead text-muted">
-                According to the National Oceanic and Atmospheric Administration, Ted, Scambos,
-                NSIDClead scentist, puts the potentially record maximum.
-              </h4>
+            <!-- Lead -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Lead Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="lead">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers. I understand culture. I am the nucleus. I think that’s
+                  a responsibility that I have, to push possibilities, to show people, this is the
+                  level that things could be at.
+                </p>
+              </div>
+            </div>
+            <!-- Blockqoute -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Quote</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <blockquote class="blockquote">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a
+                    ante.
+                  </p>
+                  <footer class="blockquote-footer">
+                    <div class="before:content-['\2014\A0'] inline-block mr-1">
+                      Someone famous in
+                    </div>
+                    <cite title="Source Title">Source Title</cite>
+                  </footer>
+                </blockquote>
+              </div>
+            </div>
+            <!-- Muted text -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Muted Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="text-muted">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers...
+                </p>
+              </div>
+            </div>
+            <!-- Primary text -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Primary Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="text-primary">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers...
+                </p>
+              </div>
+            </div>
+            <!-- Info text -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Info Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="text-info">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers...
+                </p>
+              </div>
+            </div>
+            <!-- Info text -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Success Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="text-success">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers...
+                </p>
+              </div>
+            </div>
+            <!-- Warn text -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Warning Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="text-warning">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers...
+                </p>
+              </div>
+            </div>
+            <!-- Danger text -->
+            <div class="row medium">
+              <div class="md:w-1/4">
+                <span class="title block">Danger Text</span>
+              </div>
+              <div class="md:w-3/4 pr-3">
+                <p class="text-danger">
+                  I will be the leader of a company that ends up being worth billions of dollars,
+                  because I got the answers...
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-
-    <!-- Paragraphs Section -->
-    <div class="card-typography">
-      <div class="card-header">
-        <h3 class="text-lg">Paragraphs</h3>
-      </div>
-      <div class="p-5">
-        <!-- Paragraphs -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Paragraph</span>
-          </div>
-          <div class="md:w-3/4">
-            <p>
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers. I understand culture. I am the nucleus. I think that’s a
-              responsibility that I have, to push possibilities, to show people, this is the level
-              that things could be at.
-            </p>
-          </div>
-        </div>
-        <!-- Lead -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Lead Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="lead">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers. I understand culture. I am the nucleus. I think that’s a
-              responsibility that I have, to push possibilities, to show people, this is the level
-              that things could be at.
-            </p>
-          </div>
-        </div>
-        <!-- Blockqoute -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Quote</span>
-          </div>
-          <div class="md:w-3/4">
-            <blockquote class="blockquote">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a
-                ante.
-              </p>
-              <footer class="blockquote-footer">
-                <div class="before:content-['\2014\A0'] inline-block mr-1">Someone famous in</div>
-                <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-          </div>
-        </div>
-        <!-- Muted text -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Muted Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="text-muted">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers...
-            </p>
-          </div>
-        </div>
-        <!-- Primary text -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Primary Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="text-primary">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers...
-            </p>
-          </div>
-        </div>
-        <!-- Info text -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Info Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="text-info">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers...
-            </p>
-          </div>
-        </div>
-        <!-- Info text -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Success Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="text-success">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers...
-            </p>
-          </div>
-        </div>
-        <!-- Warn text -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Warning Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="text-warning">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers...
-            </p>
-          </div>
-        </div>
-        <!-- Danger text -->
-        <div class="row medium">
-          <div class="md:w-1/4">
-            <span class="title block">Danger Text</span>
-          </div>
-          <div class="md:w-3/4">
-            <p class="text-danger">
-              I will be the leader of a company that ends up being worth billions of dollars,
-              because I got the answers...
-            </p>
-          </div>
-        </div>
-      </div>
+      </el-card>
     </div>
   </div>
 </template>
@@ -295,12 +268,15 @@ export default defineComponent({})
   @apply p-5 border-b border-primary-white;
 }
 .row {
-  @apply my-8 w-full h-auto;
+  @apply py-4 w-full h-auto break-normal;
 }
 .title {
   @apply uppercase text-80 font-semibold text-muted;
 }
 .medium {
   @apply flex flex-col md:flex-row md:justify-center md:items-center;
+}
+.row.medium p {
+  @apply mb-4;
 }
 </style>
