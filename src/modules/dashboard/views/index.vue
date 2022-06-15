ec4d7ea99483eba387f853bf0f2e413726452306
LeThanhPhongLTV <phong.lt@ltv.vn>
2022-06-15 10:25:09 +0700

    fix code smell

--- src/modules/dashboard/views/index.vue
+++ src/modules/dashboard/views/index.vue
@@ -19,71 +19,7 @@
                 </div>
               </div>
             </template>
-            <div class="card-body">
-              <!-- <div class="chart">
-                <div
-                  class="chartjs-size-monitor"
-                  style="
-                    position: absolute;
-                    inset: 0px;
-                    overflow: hidden;
-                    pointer-events: none;
-                    visibility: hidden;
-                    z-index: -1;
-                  "
-                >
-                  <div
-                    class="chartjs-size-monitor-expand"
-                    style="
-                      position: absolute;
-                      left: 0;
-                      top: 0;
-                      right: 0;
-                      bottom: 0;
-                      overflow: hidden;
-                      pointer-events: none;
-                      visibility: hidden;
-                      z-index: -1;
-                    "
-                  >
-                    <div
-                      style="
-                        position: absolute;
-                        width: 1000000px;
-                        height: 1000000px;
-                        left: 0;
-                        top: 0;
-                      "
-                    ></div>
-                  </div>
-                  <div
-                    class="chartjs-size-monitor-shrink"
-                    style="
-                      position: absolute;
-                      left: 0;
-                      top: 0;
-                      right: 0;
-                      bottom: 0;
-                      overflow: hidden;
-                      pointer-events: none;
-                      visibility: hidden;
-                      z-index: -1;
-                    "
-                  >
-                    <div
-                      style="position: absolute; width: 200%; height: 200%; left: 0; top: 0"
-                    ></div>
-                  </div>
-                </div>
-                <canvas
-                  id="chart-orders"
-                  class="chart-canvas chartjs-render-monitor"
-                  width="120"
-                  height="175"
-                  style="display: block; width: 241px; height: 350px"
-                ></canvas>
-              </div> -->
-            </div>
+            <div class="card-body">NewComponent</div>
           </el-card>
         </div>
       </div>
