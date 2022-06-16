8287d4c1a457de091a8683074b8b74387b46fe1d
Trung-Hieu-Dev <dangtrunghieusg.com>
2022-06-16 17:48:26 +0700

    fix(profile): Fix Padding & Form input | JIRA PT-161

--- src/modules/profile/views/index.vue
+++ src/modules/profile/views/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="background-url bg-top bg-cover pt-[100px] pb-[330px] md:pb-[250px] lg:pt-[100px] lg:pb-[300px] relative"
+    class="background-url bg-top bg-cover pt-[100px] pb-[330px] md:pb-[250px] lg:pt-[155px] lg:pb-[272px] relative"
   >
     <span
       class="absolute block w-full inset-0 opacity-90 bg-gradient-to-r from-indigo-850 to-indigo-950"
     ></span>
-    <div class="w-full px-4 lg:px-[30px] lg:max-w-[38%] absolute top-[15%] lg:top-1/3 break-normal">
-      <h1 class="text-white text-[2.75rem]">Hello Admin</h1>
-      <p class="text-white text-base mb-12">
+    <div class="w-full px-4 lg:px-[30px] lg:max-w-[42%] absolute top-[15%] lg:top-1/3 break-normal">
+      <h1 class="text-white text-[2.75rem] mb-2">Hello Admin</h1>
+      <p class="text-white text-base mb-12 leading-[1.7]">
         This is your profile page. You can see the progress you've made with your work and manage
         your projects or assigned tasks
       </p>
@@ -102,9 +102,10 @@
                   <el-form-item label="Email">
                     <el-input v-model="userForm.mail" placeholder="admin@argon.com" />
                   </el-form-item>
+
                   <el-form-item label="Profile photo">
                     <el-upload class="w-full">
-                      <el-input placeholder="demo.png">
+                      <el-input placeholder="demo.png" class="input-upload">
                         <template #append>Browse</template>
                       </el-input>
                     </el-upload>
@@ -175,7 +176,7 @@
               <el-link :underline="false" class="card-avatar-profile hover:scale-105">
                 <!-- Avatar -->
                 <el-avatar
-                  :size="!isHover ? 140 : 146"
+                  :size="!isHover ? 134 : 134"
                   @mouseover="hoverCheck(true)"
                   @mouseleave="hoverCheck(false)"
                   :src="imageUrl"
@@ -223,7 +224,7 @@
             </div>
             <div class="p-8">
               <div class="text-center text-card-title">
-                <h3 class="text-[13px] mt-6 font-semibold pb-2">
+                <h3 class="text-[17px] mt-6 font-semibold pb-2">
                   Admin<span class="font-thin">, 27</span>
                 </h3>
                 <h5 class="font-light">Bucharest, Romania</h5>
@@ -251,7 +252,7 @@
               <div class="flex gap-7 progress items-center">
                 <el-link
                   :underline="false"
-                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border rounded-full"
+                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border-[0.2px] rounded-full"
                 >
                   <el-avatar
                     :size="!isHover ? 48 : 48"
@@ -270,7 +271,7 @@
               <div class="flex gap-7 progress items-center">
                 <el-link
                   :underline="false"
-                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border rounded-full"
+                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border-[0.2px] rounded-full"
                 >
                   <el-avatar
                     :size="!isHover ? 48 : 48"
@@ -289,7 +290,7 @@
               <div class="flex gap-7 progress items-center">
                 <el-link
                   :underline="false"
-                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border rounded-full"
+                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border-[0.2px] rounded-full"
                 >
                   <el-avatar
                     :size="!isHover ? 48 : 48"
@@ -308,7 +309,7 @@
               <div class="flex gap-7 progress items-center">
                 <el-link
                   :underline="false"
-                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border rounded-full"
+                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border-[0.2px] rounded-full"
                 >
                   <el-avatar
                     :size="!isHover ? 48 : 48"
@@ -327,7 +328,7 @@
               <div class="flex gap-7 progress items-center">
                 <el-link
                   :underline="false"
-                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border rounded-full"
+                  class="card-avatar-profile block flex-none h-12 w-12 overflow-hidden border-[0.2px] rounded-full"
                 >
                   <el-avatar
                     :size="!isHover ? 48 : 48"
@@ -422,6 +423,9 @@ export default defineComponent({
 :deep(.progress) .el-link--inner {
   @apply border-0 #{!important};
 }
+.background-url {
+  @apply bg-[url('@/assets/images/profile-cover.jpg')] #{!important};
+}
 
 // Progress
 .el-card__body .progress {
@@ -430,7 +434,36 @@ export default defineComponent({
 .el-card__body .progress:last-child {
   @apply border-0 border-b-[#e9ecef] #{!important};
 }
-.background-url {
-  @apply bg-[url('@/assets/images/profile-cover.jpg')] #{!important};
+
+.el-form-item.el-form-item--default:last-child {
+  @apply mb-0;
+}
+
+:deep(.el-form-item__label) {
+  @apply text-dark-lighter;
+}
+
+:deep(.el-input-group__append) {
+  @apply px-3 #{!important};
+}
+
+:deep(.el-input__inner):focus {
+  .el-input-group__append {
+    @apply focus:outline-1 focus:outline-blue-400;
+  }
+}
+
+.el-card__body .progress {
+  @apply py-5 #{!important};
+}
+
+:deep(.el-input.el-input--default.el-input-group.el-input-group--append):focus-within {
+  @apply border border-blue-500 rounded #{!important};
+  &.input-upload .el-input__inner {
+    @apply focus:border-t-0 focus:border-l-0 focus:border-b-0;
+  }
+  &.input-upload .el-input-group__append {
+    @apply border-0 #{!important};
+  }
 }
 </style>
