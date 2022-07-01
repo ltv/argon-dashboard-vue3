2986e0bffef68e47901d5324e709dcaa326a6702
Dang Trung Hieu <82331523+Trung-Hieu-Dev@users.noreply.github.com>
2022-07-01 09:02:40 +0700

    fix(grid): Fix dividing components | JIRA PT-204 (#121)
    Co-authored-by: Trung-Hieu-Dev <dangtrunghieusg.com>

---
+++ src/modules/grid/views/components/EqualWidth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full">
+    <div class="grid grid-cols-6 gap-7 text-center items-center my-3">
+      <div class="grid-items col-span-3">1 of 2</div>
+      <div class="grid-items col-span-3">2 of 2</div>
+
+      <div class="grid-items col-span-2">1 of 3</div>
+      <div class="grid-items col-span-2">2 of 3</div>
+      <div class="grid-items col-span-2">3 of 3</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EqualWidth',
+})
+</script>
+
+<style scoped>
+.grid-items {
+  @apply p-3 my-1 bg-[#fff] border border-slate-200 rounded drop-shadow-[0_4px_16px_rgba(0,0,0,0.10)] shadow-[0_0_0_1px_rgb(0,0,0,0.10)] text-left text-sm text-[#393f49] border-0 font-normal;
+}
+</style>
---
+++ src/modules/grid/views/components/Mix.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full">
+    <div class="grid grid-cols-1 sm:grid-cols-12 gap-6 text-center my-3">
+      <div class="grid-items col-span-8">.col-6 .col-md-8</div>
+      <div class="grid-items col-span-4">.col-6 .col-md-4</div>
+
+      <div class="grid-items col-span-4">.col-6 .col-md-4</div>
+      <div class="grid-items col-span-4">.col-6 .col-md-4</div>
+      <div class="grid-items col-span-4">.col-6 .col-md-4</div>
+
+      <div class="grid-items col-span-6">.col-6 .col-md-6</div>
+      <div class="grid-items col-span-6">.col-6 .col-md-6</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Mix',
+})
+</script>
+
+<style scoped>
+.grid-items {
+  @apply p-3 my-1 bg-[#fff] border border-slate-200 rounded drop-shadow-[0_4px_16px_rgba(0,0,0,0.10)] shadow-[0_0_0_1px_rgb(0,0,0,0.10)] text-left text-sm text-[#393f49] border-0 font-normal;
+}
+</style>
