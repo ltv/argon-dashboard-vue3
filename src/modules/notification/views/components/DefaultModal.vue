359fa1915b7679e1044a12a85c50a4f9f479f681
Dang Trung Hieu <82331523+Trung-Hieu-Dev@users.noreply.github.com>
2022-07-06 11:07:35 +0700

    fix(notification) - Dividing modals | JIRA PT-203 (#128)
    * fix(notification) - Dividing modals | JIRA PT-203
    
    Co-authored-by: Trung-Hieu-Dev <dangtrunghieusg.com>

---
+++ src/modules/notification/views/components/DefaultModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-full">
+    <el-dialog v-model="triggerModal" title="Type your modal title" @close="$emit('closeModal')">
+      <p>
+        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+        there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
+        Semantics, a large language ocean.
+      </p>
+      <br />
+      <p>
+        A small river named Duden flows by their place and supplies it with the necessary
+        regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your
+        mouth.
+      </p>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button class="btn-open" type="primary" @click="$emit('closeModal')"
+            >Save changes</el-button
+          >
+          <el-button class="el-button--secondary" plain @click="$emit('closeModal')"
+            >Close</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DefaultModal',
+  props: ['triggerModal'],
+})
+</script>
--- src/modules/notification/views/components/Modals.vue
+++ src/modules/notification/views/components/Modals.vue
@@ -3,61 +3,21 @@
     <div class="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-6 lg:grid-cols-3 lg:gap-6">
       <!-- Modals - Default -->
       <div class="w-full">
-        <el-button type="primary" class="w-full" @click="openDefault = true">Default</el-button>
-        <el-dialog v-model="openDefault" title="Type your modal title">
-          <p>
-            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
-            there live the blind texts. Separated they live in Bookmarksgrove right at the coast of
-            the Semantics, a large language ocean.
-          </p>
-          <br />
-          <p>
-            A small river named Duden flows by their place and supplies it with the necessary
-            regelialia. It is a paradisematic country, in which roasted parts of sentences fly into
-            your mouth.
-          </p>
-          <template #footer>
-            <span class="dialog-footer">
-              <el-button class="btn-open" type="primary" @click="openDefault = false"
-                >Save changes</el-button
-              >
-              <el-button class="el-button--secondary" plain @click="openDefault = false"
-                >Close</el-button
-              >
-            </span>
-          </template>
-        </el-dialog>
+        <el-button type="primary" class="w-full" @click="openDefaultModal = !openDefaultModal"
+          >Default</el-button
+        >
+        <DefaultModal :triggerModal="openDefaultModal" @close-modal="openDefaultModal = false" />
       </div>
 
       <!-- Modal - Danger -->
       <div class="w-full">
-        <el-button type="warning" class="w-full" @click="openNotification = true"
+        <el-button type="warning" class="w-full" @click="openWarningModal = !openWarningModal"
           >Notification</el-button
         >
-        <el-dialog
-          v-model="openNotification"
-          title="Your attention is required"
-          custom-class="danger"
-        >
-          <div class="text-center py-4 text-white break-normal">
-            <el-icon><BellIcon /></el-icon>
-            <h4 class="text-white mt-6 mb-2 uppercase break-normal">You should read this!</h4>
-            <p class="text-white break-normal">
-              A small river named Duden flows by their place and supplies it with the necessary
-              regelialia.
-            </p>
-          </div>
-          <template #footer>
-            <span class="dialog-footer">
-              <el-button class="el-button--secondary" @click="openNotification = false"
-                >Ok, got it</el-button
-              >
-              <el-button class="btn1-close" type="danger" @click="openNotification = false"
-                >Close</el-button
-              >
-            </span>
-          </template>
-        </el-dialog>
+        <WarningModal
+          :triggerWarningModal="openWarningModal"
+          @close-warning-modal="openWarningModal = false"
+        />
       </div>
 
       <!-- Modals - Form -->
@@ -74,6 +34,8 @@
 <script lang="ts">
 import { defineComponent, ref, reactive } from 'vue'
 import LoginForm from 'modules/auth/views/components/LoginForm.vue'
+import DefaultModal from './DefaultModal.vue'
+import WarningModal from './WarningModal.vue'
 
 import { MailIcon, LockClosedIcon, BellIcon } from '@heroicons/vue/solid'
 
@@ -82,13 +44,15 @@ export default defineComponent({
   components: {
     BellIcon,
     LoginForm,
+    DefaultModal,
+    WarningModal,
   },
   setup() {
     // Modals - Default
-    const openDefault = ref(false)
+    const openDefaultModal = ref(false)
 
     // Modals - Notifications
-    const openNotification = ref(false)
+    const openWarningModal = ref(false)
 
     // Modals - Form
     const dialogFormVisible = ref(false)
@@ -106,12 +70,12 @@ export default defineComponent({
     }
 
     return {
-      openDefault,
-      openNotification,
+      openWarningModal,
       dialogFormVisible,
       formLabelWidth,
       form,
       onSubmit,
+      openDefaultModal,
     }
   },
 })
